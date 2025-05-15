bdf64cfac3749dfb7188a0020f5eda67e5369eea
GuyLeDouce <155856411+GuyLeDouce@users.noreply.github.com>
2025-05-15 16:21:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -504,3 +504,8 @@ async function runGauntlet(channel) {
     }
   });
 }
+client.once('ready', () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
